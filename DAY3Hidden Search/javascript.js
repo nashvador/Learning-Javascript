ea3eb0005706c606b88f422b7c43b15c0d3cbacb
nashvador <90426925+nashvador@users.noreply.github.com>
2021-11-16 15:24:43 -0600

    Day 3 Hidden search bar - click and type

---
+++ DAY3Hidden Search/javascript.js
@@ -0,0 +1,10 @@
+let inputText = document.querySelector(".input-text");
+
+document.querySelector(".icon").addEventListener("click", function () {
+  inputText.classList.toggle("active");
+  //   if (inputText.classList.contains("active")) {
+  //     inputText.classList.remove("active");
+  //   } else {
+
+  //   }
+});
